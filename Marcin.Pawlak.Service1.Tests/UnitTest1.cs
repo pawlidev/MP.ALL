74c88d2986eb4c9fd478a191df24289725f0226b
Marcin Pawlak <marcin.pawlak.agh@gmail.com>
2022-01-20 20:32:12 +0100

    Dodaj pliki projektów.

---
+++ Marcin.Pawlak.Service1.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Marcin.Pawlak.Service1.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Marcin.Pawlak.Service2.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Marcin.Pawlak.Service2.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
